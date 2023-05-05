1871b2194ca4522294550a4a5b9906e03d250503
Adrian Ortega <hello@aortegadesign.com>
2023-05-05 12:54:35 -0700

    Revamp - WIP - starts settings Rest API

--- app/http/controllers/apiController.js
+++ app/http/controllers/apiController.js
@@ -1,15 +1,37 @@
+const Settings = require("../../settings");
+const formidable = require("formidable");
 const { getWidgetItems } = require("../../api");
-const respondWithJSON = (res, data, meta = {}) => {
+
+const respondWithJSON = (res, data, meta) => {
   res.setHeader("Content-Type", "application/json");
-  res.json({ data, meta });
+  const response = { data };
+  if (meta) {
+    response.meta = meta;
+  }
+  res.json(response);
 };
 
-const widgetsApiView = (req, res) => {
+const widgetsApiHandler = (req, res) => {
   getWidgetItems().then((items) => {
     respondWithJSON(res, items);
   });
 };
 
+const settingsGetHandler = (req, res) => respondWithJSON(res, Settings.all());
+
+const settingsPostHandler = (req, res, next) => {
+  const form = formidable();
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      return next(err);
+    }
+    respondWithJSON(res, { fields, files });
+  });
+}
+
 module.exports = {
-  widgetsApiView,
+  widgetsApiHandler,
+
+  settingsGetHandler,
+  settingsPostHandler
 };
--- app/router/routes.js
+++ app/router/routes.js
@@ -1,5 +1,9 @@
 const { dashboardView } = require("../http/controllers/homeController");
-const { widgetsApiView } = require("../http/controllers/apiController");
+const {
+  widgetsApiHandler,
+  settingsGetHandler,
+  settingsPostHandler,
+} = require("../http/controllers/apiController");
 
 module.exports = [
   {
@@ -7,7 +11,17 @@ module.exports = [
     handler: dashboardView,
   },
   {
+    path: "/api/settings",
+    methods: ["GET"],
+    handler: settingsGetHandler,
+  },
+  {
+    path: "/api/settings",
+    methods: ["POST"],
+    handler: settingsPostHandler,
+  },
+  {
     path: "/api/widgets",
-    handler: widgetsApiView,
+    handler: widgetsApiHandler,
   },
 ];
